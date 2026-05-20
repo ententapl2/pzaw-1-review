1f3ee9960cad9fd53051d4433d212f58de87048f
ententapl2 <maksikx08@onet.pl>
2026-05-20 11:20:32 +0000

    zapisujesz do req.user również passhash - choć w obecnym stanie nie wycieka, to nie jest dobry wzorzec - hash powinien być tylko przy autoryzacji

--- projekt04/src/middlewares/setUser.js
+++ projekt04/src/middlewares/setUser.js
@@ -2,6 +2,7 @@ export default function setUser(userModel) {
     return function setUser(req, res, next) {
         req.user = req.session?.user_id ? userModel.getUserById(req.session.user_id) : null; // not sure about the performance of this (database access every time middleware fires)
         res.locals.user = req.user; // keep in mind this is also set in ErrorHandler.js (see comment there)
+        //req.user.passhash;
         next();
     }
 }
