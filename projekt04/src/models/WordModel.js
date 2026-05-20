a3777422b95c77267842ee536376bad05f74c504
ententapl2 <maksikx08@onet.pl>
2026-05-20 17:57:36 +0000

    (PERF) cachowanie zapytań - bardzo dobry wzorzec choć niezachowany w losowaniu słowa z powodu ograniczeń SQLITE ; ORDER BY RANDOM() jest bardzo kosztowną operacją dla dużych tabel (obliczanie losowych liczb dla każdego wiersza i sortowanie CAŁEJ tabeli), wydajniejsze choć nieidealne jest losowanie punktu w sekwencji id i wybieranie najbliższego po nim rekordu albo offset o randomowa liczbe

--- projekt04/src/models/WordModel.js
+++ projekt04/src/models/WordModel.js
@@ -148,8 +148,7 @@ export default class WordModel {
             query += " WHERE " + queryConditions.join(" AND ");
         }
 
-        query += " AND words.id >= (1 + ABS(RANDOM()) % (SELECT MAX(id) FROM words)) "
-        query += " ORDER BY words.id LIMIT 1";
+        query += " ORDER BY RANDOM() LIMIT 1";
         
 
         return db.prepare(query).get(...params);
